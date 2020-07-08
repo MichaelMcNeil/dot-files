05702a31dfe9ad9816e080a14aa1a7667ef9cd69
Michael McNeil <me@michaelmcneil.net>
2020-07-07 20:00:30 -0500

    added apps install

---
+++ apps.sh
@@ -0,0 +1,11 @@
+sudo apt-get update && sudo apt-get install -y \
+    vim-gtk \
+    tmux \
+    git \
+    xclip \
+    curl \
+    unzip
+
+git clone https://github.com/michaelmcneil/dot-files ~/repos/dot-files
+
+
